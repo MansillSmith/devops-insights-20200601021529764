6ccc5e2b679a77f4b900ae86748d1734ff71ecaf
Mansill Smith <mjs117@students.waikato.ac.nz>
2020-06-01 05:38:38 +0000

    Changed the country to NZ.

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -16,7 +16,8 @@ exports.getWeather = function(req, res) {
 		return res.status(400).send('zip missing');
 	}
 
-	var aurl = OPENWEATHERURL + '&zip=' + zip + ',us';
+	//var aurl = OPENWEATHERURL + '&zip=' + zip + ',us';
+	var aurl = OPENWEATHERURL + '&zip=' + zip + ',nz';
 
 	request({
 		method: 'GET',
