ab464e21066c97b34ca31597dc4293fbc3c28612
Mansill Smith <mjs117@students.waikato.ac.nz>
2020-06-02 06:46:23 +0000

    Added the additional table row to the output.

--- src/components/ZipResponse.js
+++ src/components/ZipResponse.js
@@ -3,13 +3,11 @@ import React from 'react';
 function ZipResponse(props) {
 
     if(props.responseData === null || props.responseData === '') {
-    	console.log("HERE1");
         return null;
     }
 
     if(props.responseData.cod === '400' || props.responseData.cod === '404') {
         setTimeout(function() { props.clearResponse();}, 20000);
-        console.log("HERE2");
         return (
             <div className="col-sm-8">
                 <div className="text-danger">{ props.responseData.message }</div>
@@ -18,8 +16,6 @@ function ZipResponse(props) {
     }
 
     if(props.responseData.cod === 200) {
-    	console.log("HERE3");
-    	/*
         return (
             <div className="col-sm-8">
                 <table className="table table-info table-hover">
@@ -51,39 +47,6 @@ function ZipResponse(props) {
                         <tr>
                             <td>Conditions</td>
                             <td>{props.responseData.weather[0].description}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        )
-        */
-       return (
-            <div className="col-sm-8">
-                <table className="table table-info table-hover">
-                    <tbody>
-                        <tr>
-                            <td>City</td>
-                            <td>{props.responseData.name}</td>
-                        </tr>
-                        <tr>
-                            <td>Temperature</td>
-                            <td>{props.responseData.main.temp}</td>
-                        </tr>
-                        <tr>
-                            <td>Pressure</td>
-                            <td>{props.responseData.main.pressure}</td>
-                        </tr>
-                        <tr>
-                            <td>Humidity</td>
-                            <td>{props.responseData.main.humidity}</td>
-                        </tr>
-                        <tr>
-                            <td>Temperature (Min)</td>
-                            <td>{props.responseData.main.temp_min}</td>
-                        </tr>
-                        <tr>
-                            <td>Temperature (Max)</td>
-                            <td>{props.responseData.main.temp_max}</td>
                         </tr>
                     </tbody>
                 </table>
