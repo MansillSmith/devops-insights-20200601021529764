a100e1c04a89edf40fb6b706722d50ae7f3d8be8
Mansill Smith <mjs117@students.waikato.ac.nz>
2020-06-03 06:43:50 +0000

    Moved the javascript to its own file

--- src/App.js
+++ src/App.js
@@ -10,11 +10,7 @@ function App() {
 	<AppHeader />
 	<AppContainer />
 	<div id="map"></div>
-	<script>
-		function initMap(){
-			var map = new google.maps.Map(document.getElementById('map'),{zoom: 8, center:{lat:175.28,lng:-37.78}})
-			}
-	</script>
+	<script src="/map.js"></script>
 	<script async defer
 		src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB54eVXdlUoy9t9GkcgUQsm7RujVuz8XIo&callback=initMap">
 	</script>
