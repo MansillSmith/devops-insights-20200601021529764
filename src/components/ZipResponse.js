aee446d45962659463dc56099c9c673379311793
Mansill Smith <mjs117@students.waikato.ac.nz>
2020-06-03 04:33:27 +0000

    Put the map inside the div

--- src/components/ZipResponse.js
+++ src/components/ZipResponse.js
@@ -50,15 +50,15 @@ function ZipResponse(props) {
                         </tr>
                     </tbody>
                 </table>
+                <div id='map' style='width: 400px; height: 300px;'></div>
+				<script>
+					mapboxgl.accessToken = 'pk.eyJ1IjoibWFuc2lsbHNtaXRod2Fpa2F0byIsImEiOiJja2F5dWo5cHkwZ2NzMnhwaTJmejQ4cGV0In0.mFF2Ye-BbHf63SpW5f8NOw';
+					var map = new mapboxgl.Map({
+					container: 'map',
+					style: 'mapbox://styles/mapbox/streets-v11'
+					});
+				</script>
             </div>
-            <div id='map' style='width: 400px; height: 300px;'></div>
-			<script>
-				mapboxgl.accessToken = 'pk.eyJ1IjoibWFuc2lsbHNtaXRod2Fpa2F0byIsImEiOiJja2F5dWo5cHkwZ2NzMnhwaTJmejQ4cGV0In0.mFF2Ye-BbHf63SpW5f8NOw';
-				var map = new mapboxgl.Map({
-				container: 'map',
-				style: 'mapbox://styles/mapbox/streets-v11'
-				});
-			</script>
         )
     }
     return null
