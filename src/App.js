42c8ffc4bea2468089d3fcf30aa038303d93c80d
Mansill Smith <mjs117@students.waikato.ac.nz>
2020-06-03 06:35:07 +0000

    Removed stuff at the end of the line

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
 	<div id="map"></div>
 	<script>
 		function initMap(){
-			new google.maps.Map(document.getElementById('map'),{zoom: 8, center:{lat:175.28,lng:-37.78}});  
-		}
+			new google.maps.Map(document.getElementById('map'),{zoom: 8, center:{lat:175.28,lng:-37.78}})
+			}
 	</script>
 	<script async defer
 		src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB54eVXdlUoy9t9GkcgUQsm7RujVuz8XIo&callback=initMap">
