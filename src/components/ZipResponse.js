62d38c394b38b0130ea25df31394993929953cfd
Mansill Smith <mjs117@students.waikato.ac.nz>
2020-06-03 05:02:22 +0000

    Rewrote the misbehaving line.

--- src/components/ZipResponse.js
+++ src/components/ZipResponse.js
@@ -55,7 +55,7 @@ function ZipResponse(props) {
 				<script>
 					mapboxgl.accessToken = 'pk.eyJ1IjoibWFuc2lsbHNtaXRod2Fpa2F0byIsImEiOiJja2F5dWo5cHkwZ2NzMnhwaTJmejQ4cGV0In0.mFF2Ye-BbHf63SpW5f8NOw';
 					var map = new mapboxgl.Map({
-					container: 'map',
+					container:'map',
 					style: 'mapbox://styles/mapbox/streets-v11'
 					});
 				</script>
