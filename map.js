a069bd26fb7314be7c4644a1306636ffebcf85cb
Mansill Smith <mjs117@students.waikato.ac.nz>
2020-06-03 07:09:39 +0000

    Added a signal if the code executed

--- map.js
+++ map.js
@@ -1,3 +1,4 @@
 function initMap(){
 	var map = new google.maps.Map(document.getElementById('map'),{zoom: 8, center:{lat:175.28,lng:-37.78}})
+	document.getElementById('title').innerHTML = 'exectued';
 }
--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function AppHeader() {
 
-    return <div>
+    return <div id="title">
         <p className="p-3 text-light h3 bg-primary">Current weather of New Zealand City</p>
     </div>
 }
